c5f9a2f4ce254f957dc847b17e39af5aaf6bd72e
Moonsu Kang <rkdms4971@naver.com>
2024-12-24 20:05:55 +0900

    [SETTING/#1] 메인 UI 추가

---
+++ feature/navigator/src/main/java/com/yapp/navigator/MainScreen.kt
@@ -0,0 +1,40 @@
+package com.yapp.navigator
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.yapp.home.homeNavGraph
+import com.yapp.mypage.mypageNavGraph
+import com.yapp.navigator.navigation.MainNavTab
+import com.yapp.navigator.navigation.MainNavigator
+import com.yapp.navigator.navigation.rememberMainNavigator
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+internal fun MainScreen(
+    modifier: Modifier = Modifier,
+    navigator: MainNavigator = rememberMainNavigator(),
+) {
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            MainBottomNavigationBar(
+                visible = navigator.shouldShowBottomBar(),
+                currentTab = navigator.currentTab,
+                entries = MainNavTab.entries.toImmutableList(),
+                onClickItem = navigator::navigate,
+            )
+        },
+    ) { innerPadding ->
+        NavHost(
+            navController = navigator.navController,
+            startDestination = navigator.startDestination,
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            homeNavGraph(padding = innerPadding)
+            mypageNavGraph(padding = innerPadding)
+        }
+    }
+}
