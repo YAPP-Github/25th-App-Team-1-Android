c16b5f310326d6bbdc3b4ed901677e8b6869beb1
dongchyeon <kimdonghyun90766@gmail.com>
2025-01-02 02:34:08 +0900

    [DESIGN/#5] 디자인시스템 Typography 정의

---
+++ core/designsystem/src/main/java/com/yapp/designsystem/theme/Font.kt
@@ -0,0 +1,12 @@
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import core.designsystem.R
+
+val Pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+)
