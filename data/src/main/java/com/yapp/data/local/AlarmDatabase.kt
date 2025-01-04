61db5eb645faa00dd2ed07e63f764a8f7831b034
dongchyeon <kimdonghyun90766@gmail.com>
2025-01-04 14:58:52 +0900

    [FEAT/#8] Hilt를 사용하여 AlarmDatabase를 DI로 제공

--- data/src/main/java/com/yapp/data/local/AlarmDatabase.kt
+++ data/src/main/java/com/yapp/data/local/AlarmDatabase.kt
@@ -1,8 +1,6 @@
 package com.yapp.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [AlarmEntity::class], version = 1, exportSchema = false)
@@ -12,20 +10,5 @@ abstract class AlarmDatabase : RoomDatabase() {
 
     companion object {
         const val DATABASE_NAME = "alarm_database"
-
-        @Volatile
-        private var INSTANCE: AlarmDatabase? = null
-
-        fun getInstance(context: Context): AlarmDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AlarmDatabase::class.java,
-                    DATABASE_NAME
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
     }
 }
---
+++ data/src/main/java/com/yapp/data/local/di/DatabaseModule.kt
@@ -0,0 +1,36 @@
+package com.yapp.data.local.di
+
+import android.content.Context
+import androidx.room.Room
+import com.yapp.data.local.AlarmDao
+import com.yapp.data.local.AlarmDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun providesAlarmDatabase(
+        context: Context
+    ): AlarmDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AlarmDatabase::class.java,
+            AlarmDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAlarmDao(
+        database: AlarmDatabase
+    ): AlarmDao {
+        return database.alarmDao()
+    }
+}
