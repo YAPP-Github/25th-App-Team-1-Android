08df6d68677ebde2d246a59a27b9f9d5269ca3e2
dongchyeon <kimdonghyun90766@gmail.com>
2025-01-15 18:48:23 +0900

    [FEAT/#40] core:common 모듈에 공용 Routes 관리

---
+++ core/common/src/main/java/com/yapp/common/navigation/Routes.kt
@@ -0,0 +1,23 @@
+package com.yapp.common.navigation
+
+object Routes {
+    object Onboarding {
+        const val ROUTE = "onboarding_route"
+        const val EXPLAIN = "onboarding_explain"
+        const val ALARM_TIME_SELECTION = "onboarding_alarm_time_selection"
+        const val BIRTHDAY = "onboarding_birthday"
+        const val TIME_OF_BIRTH = "onboarding_time_of_birth"
+        const val NAME = "onboarding_name"
+    }
+
+    object Home {
+        const val ROUTE = "home_route"
+        const val HOME = "home"
+        const val ALARM_ADD_EDIT = "alarm_add_edit"
+    }
+
+    object MyPage {
+        const val ROUTE = "mypage_route"
+        const val MYPAGE = "mypage"
+    }
+}
