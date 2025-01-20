5b5c6dd3a15deb7eab18cad94d51d04548b0b24c
Moonsu Kang <rkdms4971@naver.com>
2025-01-21 02:56:22 +0900

    [ADD/#51] 모든 화면 Full Screen 적용 (enableEdgeToEdge)

--- feature/navigator/src/main/java/com/yapp/navigator/OrbitNavHost.kt
+++ feature/navigator/src/main/java/com/yapp/navigator/OrbitNavHost.kt
@@ -1,26 +1,29 @@
 package com.yapp.navigator
 
-import androidx.compose.foundation.layout.padding
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import com.kms.onboarding.onboardingNavGraph
 import com.yapp.common.navigation.OrbitNavigator
 import com.yapp.common.navigation.destination.TopLevelDestination
 import com.yapp.common.navigation.rememberOrbitNavigator
-import com.yapp.designsystem.theme.OrbitTheme
 import com.yapp.home.homeNavGraph
+import com.yapp.mission.missionNavGraph
 import com.yapp.mypage.myPageNavGraph
 import kotlinx.collections.immutable.toImmutableList
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 internal fun OrbitNavHost(
     modifier: Modifier = Modifier,
     navigator: OrbitNavigator = rememberOrbitNavigator(),
 ) {
     Scaffold(
-        modifier = modifier,
+        modifier = modifier.fillMaxSize(),
         bottomBar = {
             OrbitBottomNavigationBar(
                 visible = false,
@@ -29,12 +32,12 @@ internal fun OrbitNavHost(
                 onClickItem = navigator::navigateToTopLevelDestination,
             )
         },
-        containerColor = OrbitTheme.colors.gray_900,
-    ) { innerPadding ->
+        containerColor = Color.Transparent,
+    ) {
         NavHost(
             navController = navigator.navController,
             startDestination = navigator.startDestination,
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier.fillMaxSize(),
         ) {
             onboardingNavGraph(
                 navigator = navigator,
@@ -44,6 +47,9 @@ internal fun OrbitNavHost(
                 navigator = navigator,
             )
             myPageNavGraph(
+                navigator = navigator,
+            )
+            missionNavGraph(
                 navigator = navigator,
             )
         }
