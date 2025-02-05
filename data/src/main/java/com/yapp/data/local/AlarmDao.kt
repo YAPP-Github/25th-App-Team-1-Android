b2cd8c93f733ee4e39010429d8491acab02abfe1
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-05 14:33:52 +0900

    [FEAT/#73] 알람 목록을 시간 순으로 정렬

--- data/src/main/java/com/yapp/data/local/AlarmDao.kt
+++ data/src/main/java/com/yapp/data/local/AlarmDao.kt
@@ -18,7 +18,7 @@ interface AlarmDao {
     @Query("SELECT * FROM ${AlarmDatabase.DATABASE_NAME} WHERE id = :id")
     suspend fun getAlarm(id: Long): AlarmEntity?
 
-    @Query("SELECT * FROM ${AlarmDatabase.DATABASE_NAME} ORDER BY id LIMIT :limit OFFSET :offset")
+    @Query("SELECT * FROM ${AlarmDatabase.DATABASE_NAME} ORDER BY isAm DESC, hour ASC, minute ASC LIMIT :limit OFFSET :offset")
     suspend fun getAlarms(limit: Int, offset: Int): List<AlarmEntity>
 
     @Query("SELECT COUNT(*) FROM ${AlarmDatabase.DATABASE_NAME}")
