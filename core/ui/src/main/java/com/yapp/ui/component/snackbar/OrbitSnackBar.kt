618eaf494b767a6d1c2ce62a740315463edd59f8
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-06 01:23:56 +0900

    [FEAT/#76] 커스텀 스낵바를 통해 기존보다 많은 정보를 설정 가능하도록 개선

--- core/ui/src/main/java/com/yapp/ui/component/snackbar/OrbitSnackBar.kt
+++ core/ui/src/main/java/com/yapp/ui/component/snackbar/OrbitSnackBar.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.yapp.designsystem.theme.OrbitTheme
 
@@ -31,50 +36,55 @@ fun OrbitSnackBar(
     containerColor: Color = OrbitTheme.colors.gray_600,
     onAction: () -> Unit,
 ) {
-    Surface(
-        modifier = modifier,
-        shape = shape,
-        color = containerColor,
-        shadowElevation = 6.dp,
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter,
     ) {
-        Row(
-            modifier = Modifier.padding(
-                horizontal = 14.dp,
-                vertical = 12.dp,
-            ),
-            verticalAlignment = Alignment.CenterVertically,
+        Surface(
+            modifier = Modifier,
+            shape = shape,
+            color = containerColor,
+            shadowElevation = 6.dp,
         ) {
-            // 아이콘 표시
-            iconRes?.let {
-                Box(
-                    modifier = Modifier
-                        .padding(end = 8.dp), // 아이콘과 메시지 간 간격 추가
-                    contentAlignment = Alignment.Center,
-                ) {
-                    Image(
-                        painter = painterResource(id = it),
-                        contentDescription = "Check Icon",
-                    )
+            Row(
+                modifier = Modifier.padding(
+                    horizontal = 14.dp,
+                    vertical = 12.dp,
+                ),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                // 아이콘 표시
+                iconRes?.let {
+                    Box(
+                        modifier = Modifier
+                            .padding(end = 8.dp), // 아이콘과 메시지 간 간격 추가
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Image(
+                            painter = painterResource(id = it),
+                            contentDescription = "Check Icon",
+                        )
+                    }
                 }
+
+                Text(
+                    color = OrbitTheme.colors.white,
+                    style = OrbitTheme.typography.label1Medium,
+                    text = message,
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    modifier = Modifier
+                        .clickable(
+                            onClick = onAction,
+                        ),
+                    color = OrbitTheme.colors.gray_50,
+                    style = OrbitTheme.typography.label2Regular,
+                    text = label,
+                )
             }
-
-            Text(
-                color = OrbitTheme.colors.white,
-                style = OrbitTheme.typography.label1Medium,
-                text = message,
-            )
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            Text(
-                modifier = Modifier
-                    .clickable(
-                        onClick = onAction,
-                    ),
-                color = OrbitTheme.colors.gray_50,
-                style = OrbitTheme.typography.label2Regular,
-                text = label,
-            )
         }
     }
 }
@@ -102,3 +112,29 @@ fun OrbitSnackBarPreview() {
         }
     }
 }
+
+suspend fun showCustomSnackBar(
+    snackBarHostState: SnackbarHostState,
+    message: String,
+    actionLabel: String? = null,
+    iconRes: Int? = null,
+    bottomPadding: Dp = 12.dp,
+): SnackbarResult {
+    return snackBarHostState.showSnackbar(
+        CustomSnackBarVisuals(
+            message = message,
+            actionLabel = actionLabel,
+            iconRes = iconRes,
+            bottomPadding = bottomPadding,
+        ),
+    )
+}
+
+data class CustomSnackBarVisuals(
+    override val message: String,
+    override val actionLabel: String? = null,
+    override val withDismissAction: Boolean = false,
+    override val duration: SnackbarDuration = SnackbarDuration.Short,
+    val iconRes: Int? = null,
+    val bottomPadding: Dp = 12.dp,
+) : SnackbarVisuals
