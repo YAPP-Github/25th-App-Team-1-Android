559fa912906c8bcfdb4475ba38bbce863a64b7d6
Moonsu Kang <rkdms4971@naver.com>
2025-02-08 05:08:46 +0900

    [REFACTOR/#88] 임시 데이터 추가 및 폰트 수정

--- feature/fortune/src/main/java/com/yapp/fortune/CodyItem.kt
+++ feature/fortune/src/main/java/com/yapp/fortune/CodyItem.kt
@@ -4,8 +4,8 @@ package com.yapp.fortune
 data class CodyItem(val resId: Int, val categoryLabel: String, val clothsNameLabel: String)
 
 fun getCodyList(): List<CodyItem> = listOf(
-    CodyItem(core.designsystem.R.drawable.ic_top, "상의", "description"),
-    CodyItem(core.designsystem.R.drawable.ic_pants, "하의", "description"),
-    CodyItem(core.designsystem.R.drawable.ic_shoes, "신발", "description"),
-    CodyItem(core.designsystem.R.drawable.ic_glove, "액세서리", "description"),
+    CodyItem(core.designsystem.R.drawable.ic_top, "상의", "베이지색 니트"),
+    CodyItem(core.designsystem.R.drawable.ic_pants, "하의", "청색 데님 팬츠"),
+    CodyItem(core.designsystem.R.drawable.ic_shoes, "신발", "흰색 스니커즈"),
+    CodyItem(core.designsystem.R.drawable.ic_glove, "액세서리", "은색 목걸이"),
 )
