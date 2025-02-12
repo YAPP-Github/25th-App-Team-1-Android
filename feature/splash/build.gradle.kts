f0599dd8ed040aa8eae73d22e27fdd8d8bfba55c
Moonsu Kang <rkdms4971@naver.com>
2025-02-13 03:45:06 +0900

    [ADD/#83] 스플래시 모듈 추가

---
+++ feature/splash/build.gradle.kts
@@ -0,0 +1,18 @@
+import com.yapp.convention.setNamespace
+
+plugins {
+    id("orbit.android.feature")
+}
+
+android {
+    setNamespace("feature.splash")
+}
+
+dependencies {
+    implementation(projects.core.ui)
+    implementation(projects.core.common)
+    implementation(projects.core.datastore)
+    implementation(libs.orbit.core)
+    implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewmodel)
+}
