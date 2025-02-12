b45ad8cb4d3a5d3497a0d98192727bfbc2e79a04
Moonsu Kang <rkdms4971@naver.com>
2025-02-12 22:19:53 +0900

    [FEAT/#110] setting에 필요한 의존성 추가

--- feature/setting/build.gradle.kts
+++ feature/setting/build.gradle.kts
@@ -11,6 +11,9 @@ android {
 dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.common)
+    implementation(projects.domain)
+    implementation(projects.core.datastore)
     implementation(libs.orbit.core)
     implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewmodel)
 }
