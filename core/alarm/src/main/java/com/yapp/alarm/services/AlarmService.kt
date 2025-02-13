0fbd0e776b2128bcef7119582ab4f2e62410e269
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-13 11:06:10 +0900

    [FEAT/#115] 알람 수신 시 AlarmInteractionActivity가 실행 중이면 종료

--- core/alarm/src/main/java/com/yapp/alarm/services/AlarmService.kt
+++ core/alarm/src/main/java/com/yapp/alarm/services/AlarmService.kt
@@ -133,6 +133,9 @@ class AlarmService : Service() {
     ): Notification {
         Log.d("AlarmForegroundService", "createNotification()")
 
+        val closeIntent = Intent(AlarmConstants.ACTION_ALARM_INTERACTION_ACTIVITY_CLOSE)
+        applicationContext.sendBroadcast(closeIntent)
+
         val alarmAlertPendingIntent =
             createAlarmAlertPendingIntent(applicationContext, alarm)
         val alarmDismissPendingIntent =
