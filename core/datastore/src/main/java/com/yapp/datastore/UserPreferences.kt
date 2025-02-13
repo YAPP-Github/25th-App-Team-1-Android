25deaf4e6ae1aebdfd13e5456d79ed411811a708
Moonsu Kang <rkdms4971@naver.com>
2025-02-14 00:28:49 +0900

    [ADD/#113] FortuneId Datastore에 담기

--- core/datastore/src/main/java/com/yapp/datastore/UserPreferences.kt
+++ core/datastore/src/main/java/com/yapp/datastore/UserPreferences.kt
@@ -4,8 +4,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.longPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -17,17 +20,33 @@ class UserPreferences @Inject constructor(
     private object Keys {
         val USER_ID = longPreferencesKey("user_id")
         val ONBOARDING_COMPLETED = booleanPreferencesKey("onboarding_completed")
+        val FORTUNE_ID = longPreferencesKey("fortune_id")
     }
 
     val userIdFlow: Flow<Long?> = dataStore.data
-        .map { preferences -> preferences[Keys.USER_ID] }
+        .catch { emit(emptyPreferences()) }
+        .map { it[Keys.USER_ID] }
+        .distinctUntilChanged()
 
     val onboardingCompletedFlow: Flow<Boolean> = dataStore.data
-        .map { preferences -> preferences[Keys.ONBOARDING_COMPLETED] ?: false }
+        .catch { emit(emptyPreferences()) }
+        .map { it[Keys.ONBOARDING_COMPLETED] ?: false }
+        .distinctUntilChanged()
+
+    val fortuneIdFlow: Flow<Long?> = dataStore.data
+        .catch { emit(emptyPreferences()) }
+        .map { it[Keys.FORTUNE_ID] }
+        .distinctUntilChanged()
 
     suspend fun saveUserId(userId: Long) {
         dataStore.edit { preferences ->
             preferences[Keys.USER_ID] = userId
+        }
+    }
+
+    suspend fun saveFortuneId(fortuneId: Long) {
+        dataStore.edit { preferences ->
+            preferences[Keys.FORTUNE_ID] = fortuneId
         }
     }
 
@@ -39,8 +58,13 @@ class UserPreferences @Inject constructor(
 
     suspend fun clearUserData() {
         dataStore.edit { preferences ->
-            preferences.remove(Keys.USER_ID)
-            preferences.remove(Keys.ONBOARDING_COMPLETED)
+            preferences.clear()
+        }
+    }
+
+    suspend fun clearFortuneId() {
+        dataStore.edit { preferences ->
+            preferences.remove(Keys.FORTUNE_ID)
         }
     }
 }
