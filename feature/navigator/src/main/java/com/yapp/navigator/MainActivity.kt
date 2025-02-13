ff9e1d210fdda4533c8b3e19e4aa8052c268484b
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-14 01:37:10 +0900

    [REMOVE/#117] handleIntent로 인한 확장 애니메이션 제거

--- feature/navigator/src/main/java/com/yapp/navigator/MainActivity.kt
+++ feature/navigator/src/main/java/com/yapp/navigator/MainActivity.kt
@@ -1,25 +1,18 @@
 package com.yapp.navigator
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.LaunchedEffect
-import androidx.navigation.NavController
 import com.yapp.common.navigation.rememberOrbitNavigator
 import com.yapp.designsystem.theme.OrbitTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private lateinit var navController: NavController
-
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,39 +29,12 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             val navigator = rememberOrbitNavigator()
-            this.navController = navigator.navController
 
             OrbitTheme {
                 OrbitNavHost(
                     navigator = navigator,
                 )
             }
-
-            LaunchedEffect(Unit) {
-                handleIntent(intent)
-            }
-        }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-
-        setIntent(intent)
-        handleIntent(intent)
-    }
-
-    private fun handleIntent(intent: Intent) {
-        val uri = intent.data
-        if (uri != null) {
-            if (::navController.isInitialized) {
-                if (!navController.handleDeepLink(intent)) {
-                    Log.e("debugging", "Failed to handle deep link: $uri")
-                }
-            } else {
-                Log.e("debugging", "NavController is not initialized yet")
-            }
-        } else {
-            Log.w("debugging", "No URI found in the intent")
         }
     }
 }
