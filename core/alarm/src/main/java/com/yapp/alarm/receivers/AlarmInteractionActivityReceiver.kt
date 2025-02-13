f17a3df90a23f88ff474de679bbdbcf063bcdc34
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-14 00:26:00 +0900

    [FEAT/#117] 알람 끄기 후 딥링크를 통한 미션 화면 이동

--- core/alarm/src/main/java/com/yapp/alarm/receivers/AlarmInteractionActivityReceiver.kt
+++ core/alarm/src/main/java/com/yapp/alarm/receivers/AlarmInteractionActivityReceiver.kt
@@ -3,6 +3,7 @@ package com.yapp.alarm.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import androidx.activity.ComponentActivity
 import com.yapp.alarm.AlarmConstants
@@ -12,6 +13,10 @@ class AlarmInteractionActivityReceiver(private val activity: ComponentActivity) 
         Log.d("AlarmInteractionActivityReceiver", "알람 수신, AlarmAlertActivity 종료")
         if (intent?.action == AlarmConstants.ACTION_ALARM_INTERACTION_ACTIVITY_CLOSE) {
             activity.finish()
+            context?.let {
+                val missionIntent = Intent(Intent.ACTION_VIEW, Uri.parse("orbitapp://mission"))
+                it.startActivity(missionIntent)
+            }
         }
     }
 }
--- feature/navigator/src/main/java/com/yapp/navigator/MainActivity.kt
+++ feature/navigator/src/main/java/com/yapp/navigator/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.yapp.navigator
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import androidx.navigation.NavController
+import com.yapp.common.navigation.rememberOrbitNavigator
 import com.yapp.designsystem.theme.OrbitTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavController
+
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +35,40 @@ class MainActivity : ComponentActivity() {
             ),
         )
         setContent {
+            val navigator = rememberOrbitNavigator()
+            this.navController = navigator.navController
+
             OrbitTheme {
-                OrbitNavHost()
+                OrbitNavHost(
+                    navigator = navigator,
+                )
             }
+
+            LaunchedEffect(Unit) {
+                handleIntent(intent)
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val uri = intent.data
+        if (uri != null) {
+            if (::navController.isInitialized) {
+                if (!navController.handleDeepLink(intent)) {
+                    Log.e("debugging", "Failed to handle deep link: $uri")
+                }
+            } else {
+                Log.e("debugging", "NavController is not initialized yet")
+            }
+        } else {
+            Log.w("debugging", "No URI found in the intent")
         }
     }
 }
