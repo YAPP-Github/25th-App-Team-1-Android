e500fe7780f70792c5e9c1d42a5da7599adee815
dongchyeon <kimdonghyun90766@gmail.com>
2025-02-13 17:16:45 +0900

    [CHORE/#115] SplashScreen LaunchedEffect import 문 누락 해결

--- feature/splash/src/main/java/com/yapp/splash/SplashScreen.kt
+++ feature/splash/src/main/java/com/yapp/splash/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
